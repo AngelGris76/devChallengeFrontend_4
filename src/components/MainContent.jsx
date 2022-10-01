7fd1c28c01358d38fc0d536f7bae098563c2c6d0
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-10-01 15:01:02 -0300

    some change on maincontent

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,11 @@
 import ACTIVE_PAGE from '../constants/activePage';
+import TaskForm from './forms/TaskForm';
 import style from './MainContent.module.css';
 
 const MainContent = ({ activePage }) => {
   return (
     <main className={style.main}>
-      {activePage !== ACTIVE_PAGE.completed && <p>formulario</p>}
+      {activePage !== ACTIVE_PAGE.completed && <TaskForm />}
       <p>Lista de tareas</p>
     </main>
   );
