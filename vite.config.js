cd7bddca247022a7edc0794d65051f599d6a1108
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-10-02 11:40:36 -0300

    some change to test subroute

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        all: resolve(__dirname, 'all.html'),
+        active: resolve(__dirname, 'active.html'),
+        completed: resolve(__dirname, 'completed.html'),
+      },
+    },
+  },
+});
